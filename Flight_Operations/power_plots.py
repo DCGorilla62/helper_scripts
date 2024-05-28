2b95f3eb37976f7b8b8da41594c4e4e3267d2ba5
Dennis H. Calderon <dennis.henry.calderon@gmail.com>
2024-05-28 09:27:39 -0400

    added power plot

---
+++ Flight_Operations/power_plots.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Read the csv files
+west_csv = pd.read_csv("830_1300_UTC/west_power.csv")
+north_csv = pd.read_csv("830_1300_UTC/north_power.csv")
+east_csv = pd.read_csv("830_1300_UTC/east_power.csv")
+south_csv = pd.read_csv("830_1300_UTC/south_power.csv")
+
